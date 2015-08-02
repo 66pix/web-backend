8e3ee7ccbaa7b3615f54b48f669901432ad273b4
Michael Robinson <mike@pagesofinterest.net>
2015-08-02 21:39:16 +1200

    Updated users plugin

--- src/plugins/66pix-users.js
+++ src/plugins/66pix-users.js
@@ -1,19 +1,67 @@
-var userModels = require('66pix-user');
+var userModels = require('66pix-users');
 module.exports = function(options) {
+
+  // Boilerplate
   var seneca = this;
-  var name = '66pix-users';
+  var pluginName = '66pix-users';
 
   options = seneca.util.deepextend({
-    prefix: '/users/'
+    prefix: '/api/users/'
   });
 
   seneca.add({
-    init: name
+    init: pluginName
   }, init);
 
+  seneca.add({
+    role: pluginName,
+    cmd: 'current'
+  }, current);
+
+  seneca.act({
+    role: 'web',
+    use: {
+      prefix: options.prefix,
+      pin: {
+        role: pluginName,
+        cmd: '*'
+      },
+      map: {
+        current: {
+          GET: true
+        }
+      }
+    }
+  });
+
+  return {
+    name: pluginName
+  };
+
+  /**
+   * Initialise user seneca plugin
+   *
+   * @param {Arguments} args
+   * @param {Function} done
+   */
   function init(args, done) {
-    console.log('init');
-    done();
+    userModels.then(function() {
+      done();
+    })
+    .catch(function(error) {
+      throw error;
+    });
+  }
+
+  // Actions
+  /**
+   * Get the currently logged in user
+   *
+   * @param {Arguments} args
+   * @param {Function} done
+   */
+  function current(args, done) {
+    done(null, {id: 123});
   }
 };
 
