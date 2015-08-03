374fa24f0d756a3401db8b50d71793a6c0813897
Michael Robinson <mike@pagesofinterest.net>
2015-08-03 23:46:47 +1200

    Moved plugins to 66pix-api

--- src/index.js
+++ src/index.js
@@ -12,92 +12,28 @@ var cookieparser = require('cookie-parser');
 var bodyparser = require('body-parser');
 var session = require('express-session');
 var csurf = require('csurf');
-var seneca = require('seneca')();
 
-// seneca.use(store, {
-//   folder: 'company',
-//   map: {
-//     '-/company/-': '*'
-//   },
-//   name: 'companies',
-//   port: 5432,
-//   host: process.env.POSTGRES_COMPANIES_HOST,
-//   username: process.env.POSTGRES_COMPANIES_USERNAME,
-//   password: process.env.POSTGRES_COMPANIES_PASSWORD
-// });
+var app = express();
 
-seneca.use('options', {
+app.use(cookieparser());
+app.use(bodyparser.json());
 
-  main: {
-    port: 3001,
-    public:'/public'
-  },
-
-  auth: {
-    sendemail: !process.env.DEVELOPMENT
-  }
-  // ,
-  // log: {
-  //     map: [
-  //     {
-  //       level: 'all',
-  //       handler: 'print'
-  //     }
-  //     ]
-  //   }
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(csurf());
+app.use(function(req, res, next) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  next();
 });
 
-seneca.use('plugins/66pix-users');
+app.use(express.static('public'));
 
-seneca.ready(function(err) {
-  if (err) {
-    return process.exit(!console.error(err));
-  }
+var server = http.createServer(app);
 
-  var options = seneca.export('options');
+server.listen(process.env.PORT);
 
-  // var u = seneca.pin({role:'user', cmd:'*'})
-  // var projectpin = seneca.pin({role:'project', cmd:'*'})
-
-  // seneca.act('role:settings, cmd:define_spec, kind:user', {spec:options.settings.spec})
-
-  var web = seneca.export('web');
-
-  var app = express();
-
-  app.use(cookieparser());
-  app.use(bodyparser.json());
-
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true
-  }));
-  // app.use(csurf());
-  // app.use(function(req, res, next) {
-  //   res.cookie('XSRF-TOKEN', req.csrfToken());
-  //   next();
-  // });
-
-  app.use(web);
-
-  // app.use(function(req, res, next) {
-  //   if (0 == req.url.indexOf('/reset') ||
-  //       0 == req.url.indexOf('/confirm')) {
-  //     req.url = '/'
-  //   }
-
-  //   next()
-  // })
-
-  var server = http.createServer(app);
-
-  // seneca.use('admin', {server:server, local:true});
-
-  server.listen(options.main.port);
-
-  seneca.log.info('listen', options.main.port);
-  seneca.log.info('listen', options.main.port);
-  seneca.listen();
-});
+require('66pix-api')(app);
 
