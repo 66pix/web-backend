790359d42291e4268737a5568f24573e3e7f8e69
Michael Robinson <mike@pagesofinterest.net>
2015-09-06 00:04:11 +1200

    Updated login.js

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -9,10 +9,12 @@ module.exports = function(app) {
       if (!req.body.email || !req.body.password) {
         return res.send(401, 'Invalid username or password');
       }
-
-      models.User.login(req.body.email, req.body.password)
+      
+      var User = models.User;
+      User.login(req.body.email, req.body.password)
         .then(function(user) {
           var token = jwt.sign({
+            id: user.id,
             email: user.email,
             name: user.name
           }, process.env.TOKEN_SECRET, {
