70ba528b27c1b185d1dbcd8457498159afcdce2a
Michael Robinson <mike@pagesofinterest.net>
2015-09-08 15:05:48 +1200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+
+npm login <<!
+$NPM_USERNAME
+$NPM_PASSWORD
+$NPM_EMAIL
+!
+npm install
+
 if [ "$NODE_ENV" == "development" ]; then
     npm run development
 else
