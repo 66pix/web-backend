67461c7f5c8908f7f3459e99641d7d1e37a75162
Michael Robinson <mike@pagesofinterest.net>
2015-09-10 22:43:19 +1200

    Removed pre push hook

--- start.sh
+++ start.sh
@@ -2,11 +2,16 @@
 set -o nounset
 set -o errexit
 
+if [ -v ${NPM_USERNAME+x} ]; then
+    cp .npmrc /root/.npmrc
+else
 npm login <<!
 $NPM_USERNAME
 $NPM_PASSWORD
 $NPM_EMAIL
 !
+fi
+
 npm install
 
 if [ "$NODE_ENV" == "development" ]; then
