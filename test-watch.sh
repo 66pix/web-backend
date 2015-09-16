8a5471e5da349907013e45aff89e2e7f7b9e0780
Michael Robinson <mike@pagesofinterest.net>
2015-09-16 21:58:18 +1200

    Updated test-watch to be nicer to use locally

--- test-watch.sh
+++ test-watch.sh
@@ -1,13 +1,66 @@
 #!/bin/bash
 
-# Required ENV
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script runs tests and watch for changes
+
+OPTIONS:
+   -h      Show this message
+   -u      postgres database user (defaults to "postgres")
+   -d      postgres host (defaults to "postgres")
+   -s      token secret (defaults to TOKEN_SECRET environment variable)
+EOF
+}
+
+DATABASE_USERNAME=
+DATABASE_HOST=
+TOKEN_SECRET=
+while getopts "hu:d:s:" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         u)
+             DATABASE_USERNAME=$OPTARG
+             ;;
+         d)
+             DATABASE_HOST=$OPTARG
+             ;;
+         s)
+             TOKEN_SECRET=$OPTARG
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+if [ -z $POSTGRES ]; then
+    export POSTGRES="postgres://$DATABASE_USERNAME@$DATABASE_HOST/test"
+fi
+
+if [ -z $DATABASE_USERNAME ]; then
+    DATABASE_USERNAME="postgres"
+fi
+
+if [ -z $DATABASE_HOST ]; then
+    DATABASE_HOST="postgres"
+fi
+
+if [ ! -z $TOKEN_SECRET ]; then
+    export TOKEN_SECRET=$TOKEN_SECRET
+fi
+
+# Required ENV for CI
 # NPM_USERNAME
 # NPM_PASSWORD
 # NPM_EMAIL
-
-set -o nounset
-set -o errexit
-
 
 if [ -v ${NPM_USERNAME+x} ]; then
     cp .npmrc /root/.npmrc
@@ -21,8 +74,8 @@ fi
 
 npm install
 
-# Ensure database exists
-! psql --host=postgres --username=postgres -c 'CREATE DATABASE test;';
+echo "! psql --host=$DATABASE_HOST --username=$DATABASE_USERNAME -c 'CREATE DATABASE test;';"
+! psql --host="$DATABASE_HOST" --username="$DATABASE_USERNAME" -c 'CREATE DATABASE test;';
 
 mkdir -p coverage
 node_modules/.bin/istanbul cover -x gulpfile.js --include-all-sources --report html ./node_modules/.bin/_mocha -- -w  --recursive --reporter spec
