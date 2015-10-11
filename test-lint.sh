7c0131add372fd815a7aadda5c071467f4911864
Michael Robinson <mike@pagesofinterest.net>
2015-10-12 12:59:30 +1300

    Whitespace between tests, use npm-install script in test.sh, renamed test.sh

--- test-lint.sh
+++ test-lint.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Required ENV
-# NPM_USERNAME
-# NPM_PASSWORD
-# NPM_EMAIL
+./npm-install.sh
 
-set -o nounset
-set -o errexit
-
-npm login <<!
-$NPM_USERNAME
-$NPM_PASSWORD
-$NPM_EMAIL
-!
 npm install
 
 # Ensure database exists
@@ -22,5 +11,11 @@ mkdir -p coverage
 /srv/www/node_modules/.bin/istanbul cover --include-all-sources -x gulpfile.js /srv/www/node_modules/.bin/_mocha -- --recursive --reporter spec
 /srv/www/node_modules/.bin/istanbul report html
 /srv/www/node_modules/.bin/istanbul report text-summary > coverage/text-summary.txt
+
+echo "\n"
+
 /srv/www/node_modules/.bin/coverage-average coverage/text-summary.txt --limit 95
 
+echo "\n"
+
+npm run lint
