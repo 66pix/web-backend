5e74fe3ebd39ac69789ef8b384baf7f969de8bab
Michael Robinson <mike@pagesofinterest.net>
2015-10-15 13:38:44 +1300

    Removing jscs

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,10 @@ var gulp = require('gulp');
 var gulpFilter = require('gulp-filter');
 var guppy = require('git-guppy')(gulp);
 var eslint = require('gulp-eslint');
-var jscs = require('gulp-jscs');
 
 function lintFiles(files) {
   return gulp.src(files)
     .pipe(gulpFilter(['*.js']))
-    .pipe(jscs({
-      configPath: '.jscsrc'
-    }))
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
