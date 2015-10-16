f26096d1b47b1daf6f11e22c603b3d3c9cdbc1d4
Michael Robinson <mike@pagesofinterest.net>
2015-10-17 00:05:05 +1300

    Updated package.json test

--- test/package.json.js
+++ test/package.json.js
@@ -9,6 +9,9 @@ describe('Package.json', function() {
   var packageJson;
   beforeEach(function(done) {
     fs.readFile(path.join(__dirname, '../package.json'), 'utf8', function(error, file) {
+      if (error) {
+        return done(error);
+      }
       packageJson = JSON.parse(file);
       done();
     });
