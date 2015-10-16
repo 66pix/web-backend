3efdef98dc9c07841abdde2961bb14e5ecd3f21a
Michael Robinson <mike@pagesofinterest.net>
2015-10-17 00:05:23 +1300

    Use cod

--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var expect = require('chai').expect;
+var expect = require('code').expect;
 
 describe('index', function() {
 
   it('should create a valid server', function(done) {
     process.env.PORT = 3333;
-    this.timeout(10000);
     require('../src/index.js').then(function(server) {
-      expect(server).to.be.defined;
-      expect(server.close).to.be.defined;
+      expect(server).to.not.be.undefined();
+      expect(server.close).to.not.be.undefined();
       server.close();
       process.env.PORT = 3000;
       done();
