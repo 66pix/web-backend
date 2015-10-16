e1b4d346bc5bead4c66ea7cd48f16462649d5b4c
Michael Robinson <mike@pagesofinterest.net>
2015-10-17 00:01:16 +1300

    Gulp can just lint commits

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,11 @@ var gulpFilter = require('gulp-filter');
 var guppy = require('git-guppy')(gulp);
 var eslint = require('gulp-eslint');
 
-function lintFiles(files) {
-  return gulp.src(files)
-    .pipe(gulpFilter(['*.js']))
+gulp.task('pre-commit', guppy.src('pre-commit', function(filesBeingCommitted) {
+  return gulp.src(filesBeingCommitted)
+  .pipe(gulpFilter(['*.js']))
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
-}
-
-gulp.task('lint', function() {
-  return lintFiles([
-    'src/**/*.js'
-  ]);
-});
-
-gulp.task('pre-commit', guppy.src('pre-commit', function(filesBeingCommitted) {
-  return lintFiles(filesBeingCommitted);
 }));
 
