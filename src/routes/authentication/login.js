551804361402992ddcd27d3551a99274404c038a
Michael Robinson <mike@pagesofinterest.net>
2015-10-29 22:04:51 +1300

    Create DB record with jwtToken, set db token id in jwttoken to allow logouts
    Card(55e66f3314c9314becd58954)

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -3,43 +3,61 @@
 var jwt = require('jsonwebtoken');
 var debug = require('debug')('authentication/login');
 
-var LOGIN_SESSION_EXPIRY = 60 * 60 * 5;
-
 module.exports = function(app) {
   require('@faceleg/66pix-models').then(function(models) {
     app.post('/authentication/login', function(req, res) {
-      if (!req.body.email || !req.body.password) {
+
+      if (bodyIsValid(req.body)) {
         return res.status(401)
-          .json({
-            code: 401,
-            message: 'Invalid email or password'
-          });
+        .json({
+          code: 401,
+          message: 'Invalid email or password'
+        });
       }
 
       var User = models.User;
+      var Token = models.Token;
       User.login(req.body.email, req.body.password)
-        .then(function(user) {
-          var token = jwt.sign({
-            id: user.id,
-            email: user.email,
-            name: user.name
-          }, process.env.TOKEN_SECRET, {
-            expiresIn: LOGIN_SESSION_EXPIRY
-          });
-
-          res.json({
-            token: token
-          });
-        })
-        .catch(User.InvalidLoginDetailsError, User.TooManyAttemptsError, function(error) {
-          debug(error);
-          res.status(error.code)
-            .json({
-              code: error.code,
-              message: error.message
-            });
+      .then(function(user) {
+        return [user, Token.build({
+          userId: user.id,
+          userAgent: req.headers['user-agent'],
+          type: 'Login',
+          isRevoked: false
+        }).save()];
+      })
+      .spread(function(user, token) {
+        var EXPIRES_IN_HOURS = 5;
+        var jwtToken = jwt.sign({
+          id: user.id,
+          tokenId: token.id
+        }, process.env.TOKEN_SECRET, {
+          expiresIn: EXPIRES_IN_HOURS + 'h',
+          issuer: '66pix Website',
+          audience: '66pix Website User'
         });
+
+        var expiresOn = new Date();
+        token.expiresOn = expiresOn.getTime() + EXPIRES_IN_HOURS * 60 * 60 * 1000;
+        return [jwtToken, token.save()];
+      })
+      .spread(function(jwtToken) {
+        res.json({
+          token: jwtToken
+        });
+      })
+      .catch(User.InvalidLoginDetailsError, User.TooManyAttemptsError, function(error) {
+        debug(error);
+        res.status(error.code)
+        .json({
+          code: error.code,
+          message: error.message
+        });
+      });
     });
   });
 };
 
+function bodyIsValid(body) {
+  return !body.email || !body.password;
+}
