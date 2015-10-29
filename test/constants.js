0c3a64e548c954b83df75151e8160237b5bccca9
Michael Robinson <mike@pagesofinterest.net>
2015-10-29 22:01:54 +1300

    Moved constants to src/
    Card(55e66f3314c9314becd58954)

--- test/constants.js
+++ test/constants.js
@@ -5,7 +5,7 @@ var expect = require('code').expect;
 describe('constants', function() {
 
   it('should expose the correct values', function() {
-    var constants = require('../constants.js');
+    var constants = require('../src/constants.js');
     expect(constants.FORGOT_PASSWORD_TOKEN_EXPIRY).to.not.be.undefined();
   });
 });
