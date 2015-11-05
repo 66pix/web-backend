141ebd50220db365357a0d17335bef03fe256a9c
Leo Sun <xsun051@hotmail.com>
2015-11-05 19:41:04 +1300

    Remove obsolete ignore

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ export TOKEN_SECRET="TOKEN_SECRET"
 export RESET_PASSWORD_TOKEN_SECRET="RESET_PASSWORD_TOKEN_SECRET"
 
 mkdir -p coverage
-/srv/www/node_modules/.bin/istanbul cover --include-all-sources -x gulpfile.js /srv/www/node_modules/.bin/_mocha -- --recursive --reporter spec
+/srv/www/node_modules/.bin/istanbul cover --include-all-sources /srv/www/node_modules/.bin/_mocha -- --recursive --reporter spec
 /srv/www/node_modules/.bin/istanbul report html
 /srv/www/node_modules/.bin/istanbul report text-summary > coverage/text-summary.txt
 
