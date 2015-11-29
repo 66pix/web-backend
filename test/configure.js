09e766b3ab33502f0330b865c3c6ecb89936b127
Michael Robinson <mike@pagesofinterest.net>
2015-11-29 23:07:30 +1300

    Drop fixtures

--- test/configure.js
+++ test/configure.js
@@ -2,12 +2,3 @@
 
 process.setMaxListeners(0);
 
-before(function(done) {
-  require('@66pix/models')
-  .then(function(models) {
-    return require('@66pix/fixtures')(models);
-  })
-  .then(function() {
-    done();
-  });
-});
