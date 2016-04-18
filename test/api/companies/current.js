34b8603beca48d56d0a39a1887d7cf0d9d9fccd4
Michael Robinson <mike@pagesofinterest.net>
2016-04-18 22:50:13 +1200

    Updated current company test

--- test/api/companies/current.js
+++ test/api/companies/current.js
@@ -2,6 +2,7 @@
 
 var expect = require('code').expect;
 var request = require('supertest');
+var Promise = require('bluebird');
 
 describe('Routes Companies current', function() {
   var app;
@@ -9,11 +10,13 @@ describe('Routes Companies current', function() {
 
   var user;
   var Company;
+  var UserAccount;
 
   beforeEach(function(done) {
     require('../../loginHelper.js')()
     .then(function(result) {
       Company = result.models.Company;
+      UserAccount = result.models.UserAccount;
       user = result.user;
       app = result.app;
       token = result.token;
@@ -24,27 +27,18 @@ describe('Routes Companies current', function() {
   });
 
   afterEach(function(done) {
-    require('@66pix/models')
-    .then(function(models) {
-      return models.UserAccount.destroy({
-        force: true,
+    Promise.all([
+      Company,
+      UserAccount
+    ].map(function(model) {
+      return model.destroy({
         truncate: true,
         cascade: true
       });
-    })
-    .then(function() {
-      return Company.destroy({
-        force: true,
-        truncate: true,
-        cascade: true
-      });
-    })
+    }))
     .then(function() {
       done();
       return null;
-    })
-    .catch(function(error) {
-      throw error;
     });
   });
 
