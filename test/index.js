76a086ba303b8192a11a6e6f1c21eb25018bd3ca
Michael Robinson <mike@pagesofinterest.net>
2016-04-18 22:49:19 +1200

    Updated index tests

--- test/index.js
+++ test/index.js
@@ -5,12 +5,10 @@ var expect = require('code').expect;
 describe('index', function() {
 
   it('should create a valid server', function(done) {
-    process.env.PORT = 3333;
     require('../src/index.js').then(function(server) {
       expect(server).to.not.be.undefined();
       expect(server.close).to.not.be.undefined();
       server.close();
-      process.env.PORT = 3000;
       done();
     });
   });
