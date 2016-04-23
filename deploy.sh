d7f2757fa2ff82662005b2d6c732d719ea02e40c
Michael Robinson <mike@pagesofinterest.net>
2016-04-23 19:14:34 +1200

    Remove npm-git-lock

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ TAG="web-backend-$ENVIRONMENT-$BRANCH-$CIRCLE_BUILD_NUM"
 
 echo ""
 echo "Installing npm dependencies"
-npm-git-lock --repo git@github.com:66pix/web-backend-node_modules.git
+./npm-login.sh && npm install
 
 echo ""
 echo "Building initial image"
