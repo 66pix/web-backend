661f4a60b3b25db5f93dc680d01604b1db75d1c5
Michael Robinson <mike@pagesofinterest.net>
2016-06-10 00:05:04 +1200

    Allow staging NODE_ENV value

--- javascript/config.js
+++ javascript/config.js
@@ -6,6 +6,7 @@ var config = convict({
         format: [
             'production',
             'development',
+            'staging',
             'test'
         ],
         default: 'development',
