5d8b846b3fdc4775d81149454602b139988e384c
Michael Robinson <mike@pagesofinterest.net>
2016-10-09 21:28:16 +1300

    Run deployments again

--- deploy.sh
+++ deploy.sh
@@ -42,26 +42,24 @@ export AWS_DEFAULT_REGION="ap-southeast-2"
 
 echo "UPLOADING TO EB"
 
-exit 0
+echo ""
+echo "Uploading deployement $DEPLOYMENT_ARTIFACT to S3"
+AWS_EB_BUCKET=elasticbeanstalk-ap-southeast-2-482348613934
+aws s3 cp $DEPLOYMENT_ARTIFACT s3://$AWS_EB_BUCKET/$DEPLOYMENT_ARTIFACT \
+  --region ap-southeast-2
 
-# echo ""
-# echo "Uploading deployement $DEPLOYMENT_ARTIFACT to S3"
-# AWS_EB_BUCKET=elasticbeanstalk-ap-southeast-2-482348613934
-# aws s3 cp $DEPLOYMENT_ARTIFACT s3://$AWS_EB_BUCKET/$DEPLOYMENT_ARTIFACT \
-#   --region ap-southeast-2
+echo ""
+echo "Creating application version"
+aws elasticbeanstalk create-application-version \
+  --application-name "${ENVIRONMENT}-66pix" \
+  --version-label "${MODULE}-${TAG_SQUASHED}" \
+  --source-bundle S3Bucket=$AWS_EB_BUCKET,S3Key=$DEPLOYMENT_ARTIFACT \
+  --region ap-southeast-2
 
-# echo ""
-# echo "Creating application version"
-# aws elasticbeanstalk create-application-version \
-#   --application-name "${ENVIRONMENT}-66pix" \
-#   --version-label "${MODULE}-${TAG_SQUASHED}" \
-#   --source-bundle S3Bucket=$AWS_EB_BUCKET,S3Key=$DEPLOYMENT_ARTIFACT \
-#   --region ap-southeast-2
-
-# echo ""
-# echo "Updating environment"
-# # Update Elastic Beanstalk environment to new version
-# aws elasticbeanstalk update-environment \
-#   --environment-name "${ENVIRONMENT}66pix-backend" \
-#   --version-label "${MODULE}-${TAG_SQUASHED}" \
-#   --region ap-southeast-2
+echo ""
+echo "Updating environment"
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment \
+  --environment-name "${ENVIRONMENT}66pix-backend" \
+  --version-label "${MODULE}-${TAG_SQUASHED}" \
+  --region ap-southeast-2
