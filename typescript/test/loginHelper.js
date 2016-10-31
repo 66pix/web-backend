8a4abf1d260b00a550386ba643937337c83c0090
Michael Robinson <mike@pagesofinterest.net>
2016-10-31 21:42:31 +1300

    Bumped all deps

--- typescript/test/loginHelper.js
+++ typescript/test/loginHelper.js
@@ -1,6 +1,5 @@
 "use strict";
 const request = require('supertest');
-const R = require('ramda');
 const models_1 = require('@66pix/models');
 const app_1 = require('../app');
 const USER_EMAIL = 'active@66pix.com';
